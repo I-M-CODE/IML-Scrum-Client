66f306c730c481434e7721f2a2f811dba04110a2
dgtyPedro <pedrogcs0104@gmail.com>
2022-03-01 09:18:20 -0300

    fix question

--- src/pages/form/form.jsx
+++ src/pages/form/form.jsx
@@ -56,7 +56,7 @@ function Form(){
         </div>
      
           <div class="grid grid-cols-1 m-5">
-            <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">O que você fez ontem?</label>
+            <label class="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">O que você fez no último dia útil?</label>
             <textarea id="input1" class="h-36 py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" type="text" />
           </div>
           <div class="grid grid-cols-1 m-5">
